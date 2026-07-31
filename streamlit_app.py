52958daf5eaea8a8255697dcdfbe17580e1de270
Conner Mongoven <214739651+mongovencm@users.noreply.github.com>
2026-07-30 21:02:43 -0500

    Improve visualizations, add date filtering, time aggregation, and summary statistics to dashboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,11 @@ metric = st.sidebar.selectbox(
     list(Files.keys())
 )
 
+time_group = st.sidebar.selectbox(
+    "Time Aggregation",
+    ["Daily", "Weekly", "Monthly"]
+)
+
 df = load_data(Files[metric])
 
 value_col = df.columns[-1]
@@ -58,10 +63,20 @@ if selected_player != "All Players":
 start_date = df["date"].min().date()
 end_date = df["date"].max().date()
 
+default_start = max(
+    df["date"].min(),
+    df["date"].max() - pd.Timedelta(days=90)
+)
+
 date_range = st.sidebar.date_input(
     "Date Range",
-    (start_date, end_date)
+    (
+        default_start.date(),
+        end_date
+    )
 )
+
+
 
 if len(date_range) == 2:
     start, end = date_range
@@ -73,25 +88,65 @@ if len(date_range) == 2:
 st.subheader(metric)
 st.caption(f"Valid range: {Score_ranges[metric]}")
 
-col1, col2 = st.columns(2)
+col1, col2, col3, col4 = st.columns(4)
 
-col1.metric("Average Score", f"{df[value_col].mean():.2f}")
-col2.metric("Records", len(df))
+col1.metric(
+    "Average",
+    f"{df[value_col].mean():.2f}"
+)
 
-daily = (
-    df.groupby("date")[value_col]
-      .mean()
-      .reset_index()
+col2.metric(
+    "Maximum",
+    f"{df[value_col].max():.2f}"
+)
+
+col3.metric(
+    "Minimum",
+    f"{df[value_col].min():.2f}"
+)
+
+col4.metric(
+    "Std Dev",
+    f"{df[value_col].std():.2f}"
+)
+
+if time_group == "Daily":
+    graph_df = (
+        df.groupby("date")[value_col]
+        .mean()
+        .reset_index()
+    )
+
+elif time_group == "Weekly":
+    graph_df = (
+        df.groupby(
+            pd.Grouper(key="date", freq="W")
+        )[value_col]
+        .mean()
+        .reset_index()
+    )
+
+else:
+    graph_df = (
+        df.groupby(
+            pd.Grouper(key="date", freq="ME")
+        )[value_col]
+        .mean()
+        .reset_index()
+    )
+
+graph_df["Rolling Average"] = (
+    graph_df[value_col]
+    .rolling(7)
+    .mean()
 )
 
 st.line_chart(
-    daily.set_index("date")
+    graph_df.set_index("date")
 )
 
-st.subheader("Filtered Data")
-
-st.dataframe(df, use_container_width=True)
-
+with st.expander("Show Filtered Data"):
+    st.dataframe(df, use_container_width=True)
 
 ### game performance data
 
@@ -99,7 +154,15 @@ game_performance_df = pd.read_csv("data/processed/game-performance/game-performa
 
 st.header("Game Performance Overview")
 
-game_performance_df["date"] = pd.to_datetime(game_performance_df["date"], errors="coerce")
+game_performance_df["date"] = pd.to_datetime(
+    game_performance_df["date"],
+    errors="coerce")
+
+game_performance_df = game_performance_df[
+    (game_performance_df["date"] >= pd.Timestamp(start))
+    &
+    (game_performance_df["date"] <= pd.Timestamp(end))
+]
 
 avg_scores = game_performance_df[[
     "team_performance",
@@ -117,17 +180,39 @@ injury_df = pd.read_csv("data/processed/injury/injury.csv")
 
 st.header("Injury Reports")
 
-injury_df["date"] = pd.to_datetime(injury_df["date"], errors="coerce")
+injury_df["date"] = pd.to_datetime(
+    injury_df["date"], 
+    errors="coerce")
 
-# group by month
-injury_counts = (
-    injury_df
-    .groupby(injury_df["date"].dt.to_period("M"))
-    .size()
-    .reset_index(name="count")
-)
+injury_df = injury_df[
+    (injury_df["date"] >= pd.Timestamp(start))
+    &
+    (injury_df["date"] <= pd.Timestamp(end))
+]
 
-injury_counts["date"] = injury_counts["date"].astype(str)
+if time_group == "Daily":
+    injury_counts = (
+        injury_df
+        .groupby("date")
+        .size()
+        .reset_index(name="count")
+    )
+
+elif time_group == "Weekly":
+    injury_counts = (
+        injury_df
+        .groupby(pd.Grouper(key="date", freq="W"))
+        .size()
+        .reset_index(name="count")
+    )
+
+else:
+    injury_counts = (
+        injury_df
+        .groupby(pd.Grouper(key="date", freq="ME"))
+        .size()
+        .reset_index(name="count")
+    )
 
 st.subheader("Injury Reports Over Time")
 
@@ -142,10 +227,38 @@ training_load_df = pd.read_csv(
 
 st.header("Training Load Overview")
 
+col1, col2, col3, col4 = st.columns(4)
+
+col1.metric(
+    "Average Load",
+    f"{training_load_df['daily_load'].mean():.0f}"
+)
+
+col2.metric(
+    "Maximum",
+    f"{training_load_df['daily_load'].max():.0f}"
+)
+
+col3.metric(
+    "Minimum",
+    f"{training_load_df['daily_load'].min():.0f}"
+)
+
+col4.metric(
+    "Std Dev",
+    f"{training_load_df['daily_load'].std():.0f}"
+)
+
 training_load_df["date"] = pd.to_datetime(
     training_load_df["date"],
     errors="coerce"
 )
+
+training_load_df = training_load_df[
+    (training_load_df["date"] >= pd.Timestamp(start))
+    &
+    (training_load_df["date"] <= pd.Timestamp(end))
+]
 
 # select athlete
 athletes = ["All Athletes"] + sorted(
@@ -167,43 +280,93 @@ if selected_athlete != "All Athletes":
 
 st.subheader("Daily Training Load")
 
-daily_training = (
-    training_load_df
-    .groupby("date")["daily_load"]
-    .mean()
-    .reset_index()
-)
+if time_group == "Daily":
+    daily_training = (
+        training_load_df
+        .groupby("date")["daily_load"]
+        .mean()
+        .reset_index()
+    )
+
+elif time_group == "Weekly":
+    daily_training = (
+        training_load_df
+        .groupby(pd.Grouper(key="date", freq="W"))["daily_load"]
+        .mean()
+        .reset_index()
+    )
+
+else:
+    daily_training = (
+        training_load_df
+        .groupby(pd.Grouper(key="date", freq="ME"))["daily_load"]
+        .mean()
+        .reset_index()
+    )
 
 st.line_chart(
     daily_training.set_index("date")
 )
+
+with st.expander("Show Training Load Data"):
+    st.dataframe(
+        training_load_df,
+        use_container_width=True
+    )
 
 
 ### training load vs injuries comparison
 
 st.subheader("Training Load vs Injury Reports")
 
-# monthly training load
-monthly_load = (
-    training_load_df
-    .groupby(training_load_df["date"].dt.to_period("M"))["daily_load"]
-    .mean()
-    .reset_index()
-)
+if time_group == "Daily":
+    monthly_load = (
+        training_load_df
+        .groupby("date")["daily_load"]
+        .mean()
+        .reset_index()
+    )
 
-monthly_load["date"] = monthly_load["date"].astype(str)
+elif time_group == "Weekly":
+    monthly_load = (
+        training_load_df
+        .groupby(pd.Grouper(key="date", freq="W"))["daily_load"]
+        .mean()
+        .reset_index()
+    )
+
+else:
+    monthly_load = (
+        training_load_df
+        .groupby(pd.Grouper(key="date", freq="ME"))["daily_load"]
+        .mean()
+        .reset_index()
+    )
 
 
-# monthly injury counts
-monthly_injuries = (
-    injury_df
-    .groupby(injury_df["date"].dt.to_period("M"))
-    .size()
-    .reset_index(name="injury_count")
-)
+if time_group == "Daily":
+    monthly_injuries = (
+        injury_df
+        .groupby("date")
+        .size()
+        .reset_index(name="injury_count")
+    )
 
-monthly_injuries["date"] = monthly_injuries["date"].astype(str)
+elif time_group == "Weekly":
+    monthly_injuries = (
+        injury_df
+        .groupby(pd.Grouper(key="date", freq="W"))
+        .size()
+        .reset_index(name="injury_count")
+    )
 
+else:
+    monthly_injuries = (
+        injury_df
+        .groupby(pd.Grouper(key="date", freq="ME"))
+        .size()
+        .reset_index(name="injury_count")
+    )
 
 # combine datasets
 load_injury_compare = monthly_load.merge(
@@ -215,8 +378,26 @@ load_injury_compare = monthly_load.merge(
 load_injury_compare = load_injury_compare.fillna(0)
 
 
+compare = load_injury_compare.copy()
+
+compare["daily_load"] = (
+    compare["daily_load"]
+    - compare["daily_load"].min()
+) / (
+    compare["daily_load"].max()
+    - compare["daily_load"].min()
+)
+
+compare["injury_count"] = (
+    compare["injury_count"]
+    - compare["injury_count"].min()
+) / (
+    compare["injury_count"].max()
+    - compare["injury_count"].min()
+)
+
 st.line_chart(
-    load_injury_compare.set_index("date")
+    compare.set_index("date")
 )
 
 
@@ -249,3 +430,263 @@ symptom_df = pd.DataFrame(
 st.subheader("Most Common Symptoms")
 
 st.bar_chart(symptom_df.set_index("symptom"))
+
+
+
+### training load vs wellness comparison
+
+st.header("Wellness vs Training Load")
+
+fatigue_df = pd.read_csv(
+    "data/processed/wellness/fatigue.csv"
+)
+
+fatigue_df["date"] = pd.to_datetime(fatigue_df["date"])
+
+fatigue_df = fatigue_df[
+    (fatigue_df["date"] >= pd.Timestamp(start))
+    &
+    (fatigue_df["date"] <= pd.Timestamp(end))
+]
+
+if time_group == "Daily":
+    daily_fatigue = (
+        fatigue_df
+        .groupby("date")["fatigue"]
+        .mean()
+        .reset_index()
+    )
+
+elif time_group == "Weekly":
+    daily_fatigue = (
+        fatigue_df
+        .groupby(pd.Grouper(key="date", freq="W"))["fatigue"]
+        .mean()
+        .reset_index()
+    )
+
+else:
+    daily_fatigue = (
+        fatigue_df
+        .groupby(pd.Grouper(key="date", freq="ME"))["fatigue"]
+        .mean()
+        .reset_index()
+    )
+
+if time_group == "Daily":
+    daily_load = (
+        training_load_df
+        .groupby("date")["daily_load"]
+        .mean()
+        .reset_index()
+    )
+
+elif time_group == "Weekly":
+    daily_load = (
+        training_load_df
+        .groupby(pd.Grouper(key="date", freq="W"))["daily_load"]
+        .mean()
+        .reset_index()
+    )
+
+else:
+    daily_load = (
+        training_load_df
+        .groupby(pd.Grouper(key="date", freq="ME"))["daily_load"]
+        .mean()
+        .reset_index()
+    )
+
+fatigue_compare = daily_load.merge(
+    daily_fatigue,
+    on="date",
+    how="inner"
+)
+
+st.subheader("Average Fatigue vs Daily Training Load")
+
+st.caption(
+    "Weekly average fatigue scores compared with average daily training load."
+)
+
+compare = fatigue_compare.copy()
+
+compare["daily_load"] = (
+    compare["daily_load"]
+    - compare["daily_load"].min()
+) / (
+    compare["daily_load"].max()
+    - compare["daily_load"].min()
+)
+
+compare["fatigue"] = (
+    compare["fatigue"]
+    - compare["fatigue"].min()
+) / (
+    compare["fatigue"].max()
+    - compare["fatigue"].min()
+)
+
+st.line_chart(
+    compare.set_index("date")
+)
+
+
+
+### sleep quality vs fatigue comparison
+sleep_df = pd.read_csv(
+    "data/processed/wellness/sleep_quality.csv"
+)
+
+sleep_df["date"] = pd.to_datetime(sleep_df["date"])
+
+sleep_df = sleep_df[
+    (sleep_df["date"] >= pd.Timestamp(start))
+    &
+    (sleep_df["date"] <= pd.Timestamp(end))
+]
+
+if time_group == "Daily":
+    sleep_daily = (
+        sleep_df
+        .groupby("date")["sleep_quality"]
+        .mean()
+        .reset_index()
+    )
+
+elif time_group == "Weekly":
+    sleep_daily = (
+        sleep_df
+        .groupby(pd.Grouper(key="date", freq="W"))["sleep_quality"]
+        .mean()
+        .reset_index()
+    )
+
+else:
+    sleep_daily = (
+        sleep_df
+        .groupby(pd.Grouper(key="date", freq="ME"))["sleep_quality"]
+        .mean()
+        .reset_index()
+    )
+
+
+if time_group == "Daily":
+    fatigue_daily = (
+        fatigue_df
+        .groupby("date")["fatigue"]
+        .mean()
+        .reset_index()
+    )
+
+elif time_group == "Weekly":
+    fatigue_daily = (
+        fatigue_df
+        .groupby(pd.Grouper(key="date", freq="W"))["fatigue"]
+        .mean()
+        .reset_index()
+    )
+
+else:
+    fatigue_daily = (
+        fatigue_df
+        .groupby(pd.Grouper(key="date", freq="ME"))["fatigue"]
+        .mean()
+        .reset_index()
+    )
+
+
+sleep_compare = sleep_daily.merge(
+    fatigue_daily,
+    on="date",
+    how="inner"
+)
+
+sleep_compare["Sleep Quality Trend"] = (
+    sleep_compare["sleep_quality"]
+    .rolling(4)
+    .mean()
+)
+
+sleep_compare["Fatigue Trend"] = (
+    sleep_compare["fatigue"]
+    .rolling(4)
+    .mean()
+)
+
+st.subheader("Average Sleep Quality vs Fatigue")
+
+st.caption(
+    "Weekly averages comparing sleep quality and fatigue with four-week trend lines."
+)
+
+st.line_chart(
+    sleep_compare.set_index("date")
+)
+
+
+### readiness vs training load comparison
+
+readiness_df = pd.read_csv(
+    "data/processed/wellness/readiness.csv"
+)
+
+readiness_df["date"] = pd.to_datetime(readiness_df["date"])
+
+readiness_df = readiness_df[
+    (readiness_df["date"] >= pd.Timestamp(start))
+    &
+    (readiness_df["date"] <= pd.Timestamp(end))
+]
+
+if time_group == "Daily":
+    daily_readiness = (
+        readiness_df
+        .groupby("date")["readiness"]
+        .mean()
+        .reset_index()
+    )
+
+elif time_group == "Weekly":
+    daily_readiness = (
+        readiness_df
+        .groupby(pd.Grouper(key="date", freq="W"))["readiness"]
+        .mean()
+        .reset_index()
+    )
+
+else:
+    daily_readiness = (
+        readiness_df
+        .groupby(pd.Grouper(key="date", freq="ME"))["readiness"]
+        .mean()
+        .reset_index()
+    )
+
+readiness_compare = daily_load.merge(
+    daily_readiness,
+    on="date",
+    how="inner"
+)
+
+readiness_compare["Readiness Trend"] = (
+    readiness_compare["readiness"]
+    .rolling(4)
+    .mean()
+)
+
+readiness_compare["Training Trend"] = (
+    readiness_compare["daily_load"]
+    .rolling(4)
+    .mean()
+)
+
+st.subheader("Average Readiness vs Daily Training Load")
+
+st.caption(
+    "Weekly averages comparing athlete readiness with training load over time."
+)
+
+st.line_chart(
+    readiness_compare.set_index("date")
+)
